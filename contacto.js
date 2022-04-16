84b4d8f468fd52f32a6461b2f53ace7a35ab55d6
Edwin <erodriguezloyaga@gmail.com>
2022-04-16 14:00:59 -0500

    Validaciones de Formulario

--- contacto.js
+++ contacto.js
@@ -1,3 +1,12 @@
 var nombre = document.getElementById("nombre")
 var correo = document.getElementById("correo")
 var asunto = document.getElementById("asunto")
+
+var form = document.getElementById("form")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    if(nombre.value.length <= 0){
+        alert("Nombre inválido")
+    }
+})
