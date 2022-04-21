edae4fb19ddfe899bbf8df84611251a4f4495bb9
Edwin <erodriguezloyaga@gmail.com>
2022-04-20 20:30:57 -0500

    Validaciones de contacto

--- contacto.js
+++ contacto.js
@@ -1,12 +1,24 @@
-var nombre = document.getElementById("nombre")
-var correo = document.getElementById("correo")
-var asunto = document.getElementById("asunto")
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const asunto = document.getElementById("asunto");
+const mensaje = document.getElementById('mensaje');
 
 var form = document.getElementById("form")
 
 form.addEventListener("submit", e=>{
     e.preventDefault()
     if(nombre.value.length <= 0){
-        alert("Nombre inválido")
+        nombre.style.border = "1px solid red";
+    }
+
+    if(correo.value.length <= 0){
+        correo.style.border = "1px solid red";
+    }
+
+    if(asunto.value.length <= 0){
+        asunto.style.border = "1px solid red";
+    }
+    if(mensaje.value.length <= 0){
+        mensaje.style.border = "1px solid red";
     }
 })
